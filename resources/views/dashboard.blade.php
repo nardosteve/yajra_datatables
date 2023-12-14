0517becfd1e7b2863c7f2235f817c3d3cf10b343
Nardo Steve <jackcale50@gmail.com>
2023-12-14 12:18:35 +0300

    Getting started with yajra datatables

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,40 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{-- <table class="table mb-5">
+                        <thead>
+                          <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Date Joined</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($users as $user)
+                                <tr>
+                                    <th scope="row">{{ $user->id }}</th>
+                                    <td>{{ $user->name }}</td>
+                                    <td>{{ $user->email }}</td>
+                                    <td>{{ $user->created_at }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>   
+                    {{ $users->links() }}    --}}
+
+                    {{ $dataTable->table() }}
+                </div>
+            </div>
+        </div>
+    </div>
+    {{ $dataTable->scripts(attributes: ['type' => 'module']) }}
+</x-app-layout>
